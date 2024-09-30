2bf2bb6e19ffe9d744377cddbf9d600f6fd6bf42
Ralph Göstenmeier <ralphg@server.local>
2024-09-30 08:00:57 +0200

    -

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+export ENV_NAME=Vue-Playground
+export ENV_HOME=$PWD
+export     HOME=$ENV_HOME
+
+. venv env        init
+. venv vscode     init
+
+. venv node       init 22.9.0
+. venv composer   init 
